7a11712b8297dd1b1aa3c9c7d5ad3d0ecb8789a0
6uf <84757238+suffz@users.noreply.github.com>
2023-11-09 19:46:25 -0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/suffz/cfws
+
+go 1.21
+
+require (
+	github.com/bogdanfinn/utls v1.5.16
+	github.com/gorilla/websocket v1.5.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ ws.go
@@ -0,0 +1,73 @@
+package cfws
+
+import (
+	"net"
+	"net/http"
+	"net/url"
+	"strings"
+
+	tls "github.com/bogdanfinn/utls"
+	gorilla "github.com/gorilla/websocket"
+)
+
+/*
+func main() {
+	conn := (&WebsocketOptions{
+		URL:        "wss://ws.bloxflip.com/socket.io/?EIO=3&transport=websocket",
+		ServerName: "ws.bloxflip.com", PORT: "443",
+		Origin:    "https://bloxflip.com",
+		Host:      "ws.bloxflip.com",
+		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203",
+		// Extensions: "permessage-deflate; client_max_window_bits",
+	}).Dial()
+	for {
+		_, msg, err := conn.Conn.ReadMessage()
+		fmt.Println(string(msg), err)
+	}
+}
+*/
+
+type WebsocketOptions struct {
+	URL              string
+	ServerName, PORT string
+	Origin           string
+	Host             string
+	Extensions       string
+	UserAgent        string
+}
+
+type WebsocketConnection struct {
+	Conn *gorilla.Conn
+	Resp *http.Response
+	Err  error
+}
+
+func (Info *WebsocketOptions) Dial() WebsocketConnection {
+
+	i, err := url.Parse(Info.URL)
+	if err != nil {
+		return WebsocketConnection{
+			Err: err,
+		}
+	}
+
+	if conn, err := net.Dial("tcp", Info.ServerName+":"+strings.ReplaceAll(Info.PORT, ":", "")); err == nil {
+		conn, resp, err := gorilla.NewClient(tls.UClient(conn, &tls.Config{
+			ServerName: Info.ServerName,
+		}, tls.HelloChrome_112, true, true).NetConn(), i, map[string][]string{
+			"Origin":                   {Info.Origin},
+			"Host":                     {Info.Host},
+			"User-Agent":               {Info.UserAgent},
+			"Sec-WebSocket-Extensions": {Info.Extensions},
+		}, 5*1024*1024, 5*1024*1024) // 5mb of allocated storage.
+		return WebsocketConnection{
+			Conn: conn,
+			Resp: resp,
+			Err:  err,
+		}
+	} else {
+		return WebsocketConnection{
+			Err: err,
+		}
+	}
+}
